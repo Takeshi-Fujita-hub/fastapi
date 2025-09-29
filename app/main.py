c2b14dcc37a13ac6917b8e173190b6cc00a31d67
Nekotarou395 <shin.shibusawa.jichikai@gmail.com>
2025-09-29 20:42:48 +0900

    1st

--- app/main.py
+++ app/main.py
@@ -5,15 +5,3 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI (read-only) 123"}
-
-@app.get("/healthz")
-def health_check():
-    return {"status": "ok"}
-
-@app.get("/items/")
-def read_items():
-    return [
-        {"name": "apple"},
-        {"name": "banana"},
-        {"name": "cherry"}
-    ]
